6ceb226afecd876b3d51f2293994a14a1147a82c
unknown <royaam@code.edu.az>
2022-12-11 15:33:16 +0400

    task7

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -110,3 +110,18 @@ static void Count(int n, int m)
     Console.WriteLine(sum);
 }
 
+
+Sum2(1, 5);
+static void Sum2(int n, int m)
+{
+    int sum = 0;
+    for (int i = n; i < m; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i;
+        }
+    }
+    Console.WriteLine(sum);
+}
+
