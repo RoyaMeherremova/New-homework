9db2de0c266be6e1f2c0cc5d00fae6073ef5d85f
unknown <royaam@code.edu.az>
2022-12-11 15:22:27 +0400

    task1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+Num(21);
+static void Num(int n)
+
+{
+    if (n % 3 == 0 && n % 7 == 0)
+
+    {
+        Console.WriteLine("bolunur");
+    }
+    else
+    {
+        Console.WriteLine("bolunmur");
+    }
+
+
+}
